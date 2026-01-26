88242515be503ff5e334660defe913e0efdc3613
sucreistaken <kadiraytarsus33@gmail.com>
2026-01-26 23:37:34 +0300

    chore: Configure client port 5173 and revert automatic sibling fetching

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
